b11497c5404d4ba1561af0e3500e1e600847cf4a
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2019-01-06 14:08:53 +0100

    Use slab instead of BTreeMap to manage tokens

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::net::{SocketAddr, IpAddr, Ipv4Addr, Shutdown};
 use std::io::{ErrorKind, Read, Write, stdout};
-use std::collections::btree_map::{BTreeMap, Entry};
 use std::{usize,u16};
 
 extern crate mio;
 use mio::{Token, Poll, PollOpt, Ready, Events};
 use mio::net::{TcpListener, TcpStream};
 
+extern crate slab;
+use slab::Slab;
 
 const DISCARD_PORT: u16 = 9; // only need to read
 const QOTD_PORT: u16 = 17; // only need to write
@@ -25,12 +26,12 @@ enum TcpConnection {
 }
 
 struct Streams {
-    prev_token: Token,
-    streams: BTreeMap<Token,(TcpStream,TcpConnection)>, // TODO use slab probably
+    min_token: Token,
+    streams: Slab<(TcpStream,TcpConnection)>,
 }
 impl Streams {
     fn new(min_token: Token) -> Self {
-        Streams { prev_token: min_token,  streams: BTreeMap::new() }
+        Streams { min_token: min_token,  streams: Slab::with_capacity(1024) }
     }
     fn add(&mut self,  source: &TcpListener,  typ: TcpConnection,  look_for: Ready, poll: &Poll) {
         loop {
@@ -38,19 +39,29 @@ impl Streams {
                 Err(ref e) if e.kind() == ErrorKind::WouldBlock => break,
                 Err(e) => eprintln!("Error accepting echo TCP connection: {}", e),
                 Ok((stream, _addr)) => {
-                    self.prev_token.0 += 1;
-                    if self.prev_token.0 == usize::MAX {
-                        panic!("Too many streams have been opened");
-                        // TODO reregister all
+                    let key = self.streams.insert((stream, typ));
+                    let token = Token(key+self.min_token.0);
+                    let stream = &self.streams[key].0;
+                    if let Err(e) = poll.register(stream, token, look_for, PollOpt::edge()) {
+                        eprintln!("Cannot register TCP connection: {}", e);
+                        self.streams.remove(key);
                     }
-                    let token = self.prev_token;
-                    match poll.register(&stream, token, look_for, PollOpt::edge()) {
-                        Err(e) => eprintln!("Cannot register TCP connection: {}", e),
-                        Ok(()) => {self.streams.insert(token, (stream, typ));}
-                    }
-
                 }
             }
+        }
+    }
+    fn get_mut(&mut self,  token: Token) -> Option<&mut(TcpStream,TcpConnection)> {
+        self.streams.get_mut(token.0-self.min_token.0)
+    }
+    fn remove(&mut self,  token: Token,  poll: &Poll) {
+        let (stream, _) = self.streams.remove(token.0-self.min_token.0);
+        // TODO if length < 1/4 capacaity and above some minimum, recreate slab and reregister;
+        // shrink_to_fit() doesn't do much: https://github.com/carllerche/slab/issues/38
+        if let Err(e) = stream.shutdown(Shutdown::Both) {
+            eprintln!("Error shutting down {:?}: {}", stream, e);
+        }
+        if let Err(e) = poll.deregister(&stream) {
+            eprintln!("Error deregistering {:?}: {}", stream, e);
         }
     }
 }
@@ -85,45 +96,32 @@ fn main() {
 
     loop {
         poll.poll(&mut events, None).expect("Cannot poll selector");
-        println!("connections: {}, events: {}, last token: {}", streams.streams.len(), events.len(), streams.prev_token.0);
+        println!("connections: {}, events: {}", streams.streams.len(), events.iter().count());
         for event in events.iter() {
             println!("\t{:?}", event);
             match event.token() {
                 DISCARD_TCP => streams.add(&discard_server, TcpConnection::Discard, Ready::readable(), &poll),
                 QOTD_TCP => streams.add(&qotd_server, TcpConnection::Qotd{sent: 0}, Ready::writable(), &poll),
                 conn_token => {
-                    let mut oe = match streams.streams.entry(conn_token) {
-                        Entry::Occupied(oe) => oe,
-                        Entry::Vacant(_) => {
+                    let (stream, typ) = match streams.get_mut(conn_token) {
+                        Some(oe) => oe,
+                        None => {
                             eprintln!("Unknown mio token: {}", conn_token.0);
                             continue;
                         }
                     };
-                    let (stream, typ) = oe.get_mut();
                     match typ {
                         TcpConnection::Discard => loop {
                             match stream.read(&mut discard_buf) {
                                 Err(ref e) if e.kind() == ErrorKind::WouldBlock => break,
                                 Err(e) => {
                                     eprintln!("Error reading data to discard: {}, closing", e);
-                                    let (stream, _) = oe.remove();
-                                    if let Err(e) = stream.shutdown(Shutdown::Both) {
-                                        eprintln!("Error shutting down {:?}: {}", stream, e);
-                                    }
-                                    if let Err(e) = poll.deregister(&stream) {
-                                        eprintln!("Error deregistering {:?}: {}", stream, e);
-                                    }
+                                    streams.remove(conn_token, &poll);
                                     break;
                                 }
                                 Ok(0) => {
                                     eprintln!("ending {:?}", stream);
-                                    let (stream, _) = oe.remove();
-                                    if let Err(e) = stream.shutdown(Shutdown::Both) {
-                                        eprintln!("Error shutting down {:?}: {}", stream, e);
-                                    }
-                                    if let Err(e) = poll.deregister(&stream) {
-                                        eprintln!("Error deregistering {:?}: {}", stream, e);
-                                    }
+                                    streams.remove(conn_token, &poll);
                                     break;
                                 }
                                 Ok(len) => {
@@ -137,35 +135,17 @@ fn main() {
                                 Err(ref e) if e.kind() == ErrorKind::WouldBlock => break,
                                 Err(e) => {
                                     eprintln!("Error sending qotd: {}, closing", e);
-                                    let (stream, _) = oe.remove();
-                                    if let Err(e) = stream.shutdown(Shutdown::Both) {
-                                        eprintln!("Error shutting down {:?}: {}", stream, e);
-                                    }
-                                    if let Err(e) = poll.deregister(&stream) {
-                                        eprintln!("Error deregistering {:?}: {}", stream, e);
-                                    }
+                                    streams.remove(conn_token, &poll);
                                     break;
                                 }
                                 Ok(0) => {
-                                    let (stream, _) = oe.remove();
-                                    if let Err(e) = stream.shutdown(Shutdown::Both) {
-                                        eprintln!("Error shutting down {:?}: {}", stream, e);
-                                    }
-                                    if let Err(e) = poll.deregister(&stream) {
-                                        eprintln!("Error deregistering {:?}: {}", stream, e);
-                                    }
+                                    streams.remove(conn_token, &poll);
                                     break;
                                 }
                                 Ok(len) => {
                                     *sent += len;
                                     if *sent >= QOTD.len() {
-                                        let (stream, _) = oe.remove();
-                                        if let Err(e) = stream.shutdown(Shutdown::Both) {
-                                            eprintln!("Error shutting down {:?}: {}", stream, e);
-                                        }
-                                        if let Err(e) = poll.deregister(&stream) {
-                                            eprintln!("Error deregistering {:?}: {}", stream, e);
-                                        }
+                                        streams.remove(conn_token, &poll);
                                         break;
                                     }
                                 }
